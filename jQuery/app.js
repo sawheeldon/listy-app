fbdf329f6cee4cd86f94bc37184d2d3e7ab2a513
sawheeldon <simon@jampot.design>
2016-05-05 23:42:29 +0100

    Finalised CSS and HTML

---
+++ jQuery/app.js
@@ -0,0 +1,9 @@
+/* Step One define functions*/
+//add item to list
+//be able to tick once an item has been bought, strike through item
+//be able to cross item off and item enters bin
+//delete all items
+
+
+
+/* Step Two Using the functions */
